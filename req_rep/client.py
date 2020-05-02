55ed1b8b4c86711180451badb8aa7007d244d0d7
ronek22 <jronek3010@gmail.com>
2020-05-02 17:17:05 +0200

    Changed to OOP for Client code

--- req_rep/client.py
+++ req_rep/client.py
@@ -4,45 +4,52 @@ from time import sleep
 import argparse
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--ip', type=str, required=True)
-args = parser.parse_args()
+class Client:
+    def __init__(self, ip, port=5555):
+        self.context = zmq.Context()
+        self.req = context.socket(zmq.REQ).connect(f'tcp://{ip}:{port}')
+        self.pub = context.socket(zmq.PUB).connect(f'tcp://{ip}:{port-1}')
+        self.id = uuid.uuid4().urn[9:]
 
-port = 5555
-context = zmq.Context()
-req = context.socket(zmq.REQ)
-req.connect(f'tcp://{args.ip}:{port}')
+    def send_using_pub(self, message='Hello'):
+        if self.pub: 
+            print(f"Client: {self.id}\nSending request to server.")
+            self.pub.send_multipart([bytes(self.id, 'utf-8'), bytes(message, 'utf-8')])
 
-pub = context.socket(zmq.PUB)
-pub.connect(f'tcp://{args.ip}:{port-1}')
 
-id = uuid.uuid4().urn[9:]
+    def send_using_req(self, message='Hello'):
+        if self.req:
+            print(f"Sending message to subscriber: {message}") 
+            self.req.send_multipart([bytes(self.id,'utf-8'), bytes(message, 'utf-8')])
 
-def send_using_req(message='Hello'):
-    if req: 
-        print(f"Client: {id}\nSending request to server.")
-        socket.send_multipart([bytes(id, 'utf-8'), bytes(message, 'utf-8')])
+            reply = self.req.recv()
+            print(f"Received reply: {reply}")
 
-def send_using_pub(message='Hello'):
-    if pub:
-        print(f"Sending message to subscriber: {message}") 
-        pub.send_multipart([bytes(id,'utf-8'), bytes(message, 'utf-8')])
+    def test(self):
+        for request in range(10):
+            print(f"Sending request no. {request}")
+            self.req.send_multipart([bytes(self.id, 'utf-8'), b'Hello'])
 
-        reply = socket.recv()
-        print(f"Received reply: {reply}")
+            # Get reply,
+            # If server is not working that maybe blocking, lecturer account, timeout and cancel
+            # TODO: TIMEOUT FUNCTION
+            reply = self.req.recv()
+            print(f"Received reply:  {reply}")
 
-def test():
-    for request in range(10):
-        print(f"Sending request no. {request}")
-        socket.send_multipart([bytes(id, 'utf-8'), b'Hello'])
+            sleep(.1)
 
-        # Get reply,
-        # If server is not working that maybe blocking, lecturer account, timeout and cancel
-        # TODO: TIMEOUT FUNCTION
-        reply = socket.recv()
-        print(f"Received reply:  {reply}")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', type=str, required=True)
+    args = parser.parse_args()
 
-        sleep(.1)
+    client = Client(args.ip)
+
+
+
+
+
+
 
 
 
