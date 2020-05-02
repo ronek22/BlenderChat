895d6c655bb27365d06328fe011b5990a5fb1511
ronek22 <jronek3010@gmail.com>
2020-05-02 16:44:40 +0200

    Added argumentParser for client

--- req_rep/client.py
+++ req_rep/client.py
@@ -1,21 +1,32 @@
 import zmq
 import uuid
 from time import sleep
+import argparse
 
-port = 5555
-context = zmq.Context()
-socket = context.socket(zmq.REQ)
-socket.connect(f'tcp://188.68.236.117:{port}')
-id = uuid.uuid4().urn[9:]
 
-for request in range(10):
-    print(f"Sending request no. {request}")
-    socket.send_multipart([bytes(id, 'utf-8'), b'Hello'])
 
-    # Get reply,
-    # If server is not working that maybe blocking, lecturer account, timeout and cancel
-    # TODO: TIMEOUT FUNCTION
-    reply = socket.recv()
-    print(f"Received reply:  {reply}")
 
-    sleep(.1)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ip', type=str, required=True)
+    args = parser.parse_args()
+
+    port = 5555
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect(f'tcp://{args.ip}:{port}')
+    id = uuid.uuid4().urn[9:]
+
+    for request in range(10):
+        print(f"Sending request no. {request}")
+        socket.send_multipart([bytes(id, 'utf-8'), b'Hello'])
+
+        # Get reply,
+        # If server is not working that maybe blocking, lecturer account, timeout and cancel
+        # TODO: TIMEOUT FUNCTION
+        reply = socket.recv()
+        print(f"Received reply:  {reply}")
+
+        sleep(.1)
